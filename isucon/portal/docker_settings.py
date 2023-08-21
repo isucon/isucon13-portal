66ed9d7ce2a70a0abd1b9862f0e433a06a8e4a2d
chibiegg <chibiegg@chibiegg.net>
2023-08-21 15:08:08 +0900

    AWS_STORAGE_BUCKET_NAMEを環境変数から取得

--- isucon/portal/docker_settings.py
+++ isucon/portal/docker_settings.py
@@ -67,7 +67,7 @@ STATIC_ROOT = "/opt/app/static/"
 # AWS
 AWS_ACCESS_KEY_ID = ""
 AWS_SECRET_ACCESS_KEY = ""
-AWS_STORAGE_BUCKET_NAME = "isucon13-portal-dev"
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "isucon13-portal-dev")
 
 # 動作設定
 
