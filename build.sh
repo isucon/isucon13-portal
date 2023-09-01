e07f1d8d16d45be7e6983235bbde9f3c1b77367d
chibiegg <chibiegg@chibiegg.net>
2023-09-01 13:13:32 +0900

    ランディングページに記載の文言を静的に

--- build.sh
+++ build.sh
@@ -9,5 +9,6 @@ if [ -z "$TAG" ];then
 fi
 
 docker build --platform linux/amd64 -t ${IMAGE}:${TAG} .
+aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 424484851194.dkr.ecr.ap-northeast-1.amazonaws.com
 docker push ${IMAGE}:${TAG}
 
