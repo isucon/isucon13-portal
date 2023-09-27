cad26b40ae1b71576a6c1daa801c84b12c332c60
chibiegg <chibiegg@chibiegg.net>
2023-09-27 10:57:53 +0900

    本番のDiscordの設定

--- isucon/portal/production_settings.py
+++ isucon/portal/production_settings.py
@@ -11,3 +11,13 @@ REGISTRATION_END_AT = portal_utils.get_jst_datetime(2023, 9, 9, 10, 10, 0)
 
 # 最大チーム数
 MAX_TEAM_NUM = 220 + 200 + 200
+
+# Discord
+DISCORD_APPLICATION_ID = os.environ.get("DISCORD_APPLICATION_ID", "")
+DISCORD_SECRET = os.environ.get("DISCORD_SECRET", "")
+DISCORD_OAUTH_CLIENT_ID = os.environ.get("DISCORD_OAUTH_CLIENT_ID", "")
+DISCORD_OAUTH_CLIENT_SECRET = os.environ.get("DISCORD_OAUTH_CLIENT_SECRET", "")
+
+DISCORD_SERVER_ID = os.environ.get("DISCORD_SERVER_ID", "")
+DISCORD_BOT_ACCESS_TOKEN = os.environ.get("DISCORD_BOT_ACCESS_TOKEN", "")
+DISCORD_USER_ROLE_ID = os.environ.get("DISCORD_USER_ROLE_ID", "")
