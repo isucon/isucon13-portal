256c55d036d350c3e99f6aea4013d39e90827ccd
chibiegg <chibiegg@chibiegg.net>
2023-11-18 00:08:28 +0900

    サーバ登録

--- isucon/portal/contest/urls.py
+++ isucon/portal/contest/urls.py
@@ -5,12 +5,12 @@ from isucon.portal.contest import views
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
+    path('cloudformation_contest.yaml', views.cloudformation_contest, name="cloudformation_contest"),
     path('jobs/', views.jobs, name="jobs"),
     path('jobs/enqueue/', views.job_enqueue, name="job_enqueue"),
     path('jobs/<int:pk>/', views.job_detail, name="job_detail"),
     path('scores/', views.scores, name="scores"),
     # path('servers/', views.servers, name="servers"),
-    # path('servers/<int:pk>/', views.delete_server, name="delete_server"),
     # path('graph/', views.graph, name="graph"),
     # path('teams/', views.teams, name="teams"),
     # path('staff/', include("isucon.portal.contest.staff.urls")),
